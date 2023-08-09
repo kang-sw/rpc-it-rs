45ad21c59c1f488f7eecc41fd2e1750a278d79ce
kang-sw <ki6080@gmail.com>
2023-08-09 22:35:52 +0900

    Define transport traits

--- src/lib.rs
+++ src/lib.rs
@@ -26,27 +26,66 @@ pub extern crate erased_serde;
 pub extern crate serde;
 
 pub mod transport {
-    use std::pin::Pin;
+    use core::{pin::Pin, task::Poll};
+    use std::task::Context;
 
     /// Asynchronously writes data frame to underlying transport.
     ///
-    /// This doesn't need to be cancellation safe as long as it cleans up internal state
-    /// on every `poll_pre_write` call. This doesn't need to implement unpin, as it'll always
-    /// be used in a pinned context(inside boxed context).
-    ///
-    /// This may be accessed from multiple thread/context.
+    /// This may be accessed from multiple thread/context. This is cancellation unsafe, as it leaves
+    /// the stream out in invalid state if the task in cancelled during
+    /// [`AsyncWriteFrame::poll_write`] is being handled.
     pub trait AsyncWriteFrame: Send + Sync + 'static {
-        // fn poll_pre_write(self: Pin<&mut self>) ->
+        fn poll_pre_write(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+        ) -> Poll<Result<(), std::io::Error>> {
+            let _ = cx;
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_write(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buffer: &[u8],
+        ) -> Poll<Result<(), std::io::Error>>;
     }
 
-    /// Asynchronously reads data frame from underlying transport.
-    pub trait AsyncReadFrame: Send + 'static {}
+    /// Asynchronously reads data frame from underlying transport. This doesn't need to be
+    /// cancellation safe, since cancellation means connection expiration, thus
+    /// [`AsyncReadFrame::poll_read`] is never called again.
+    pub trait AsyncReadFrame: Send + 'static {
+        /// Polls the underlying transport for frame read.
+        ///
+        /// # Returns
+        ///
+        /// - `Poll::Ready(Ok(frame_length))` if single frame is successfully read.
+        /// - `Poll::Ready(Err(...))` if any error occurs during read.
+        /// - `Poll::Pending` if the underlying transport is not ready.
+        fn poll_read(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            write: &mut dyn std::io::Write,
+        ) -> Poll<Result<usize, std::io::Error>>;
+    }
 
     /// Asynchronously reads data stream from underlying transport.
     ///
     /// This should be combined with [`crate::codec::Framing`] to split the stream into frames.
     /// This is shameless conceptual copy of `AsyncRead` trait from `tokio`/`futures` crate.
-    pub trait AsyncRead: Send + 'static {}
+    pub trait AsyncRead: Send + 'static {
+        /// Polls the underlying transport for data read.
+        ///
+        /// # Returns
+        ///
+        /// - `Poll::Ready(Ok(bytes_read))` if data is successfully read.
+        /// - `Poll::Ready(Err(...))` if any error occurs during read.
+        /// - `Poll::Pending` if the underlying transport is not ready.
+        fn poll_read(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buffer: &mut [u8],
+        ) -> Poll<Result<usize, std::io::Error>>;
+    }
 }
 
 pub mod codec {
@@ -60,9 +99,34 @@ pub mod codec {
 
     /// Splits data stream into frames. For example, for implmenting JSON-RPC over TCP,
     /// this would split the stream into JSON-RPC objects delimited by objects.
-    pub trait Framing {}
+    pub trait Framing {
+        /// Advance internal parsing status
+        ///
+        /// # Returns
+        ///
+        /// - `Ok(Some(Range))` if a frame is found. The range is the range of the frame from the
+        ///   beginning of the input buffer. After returning valid range, from the next
+        ///   [`Self::advance`] call the buffer should be sliced from the end of the range.
+        /// - `Ok(None)` if a frame is not found. The buffer should be kept as-is, and the next
+        ///   [`Self::advance`] call should be called with the same buffer, but extended with more
+        ///   data from the underlying transport.
+        /// - `Err(...)` if any error occurs during framing.
+        fn advance(&mut self, buffer: &[u8]) -> Result<Option<Range<usize>>, FramingError>;
+    }
 
-    /// Parses/Encodes data frame
+    #[derive(Debug, thiserror::Error)]
+    pub enum FramingError {
+        #[error("Broken buffer. The connection should be closed. Context: {0}")]
+        BrokenBuffer(Cow<'static, str>),
+
+        #[error("Error occurred, but internal state can be restored after {0} bytes")]
+        Recoverable(usize),
+    }
+
+    /// Parses/Encodes data frame.
+    ///
+    /// This is a trait that encodes/decodes data frame into underlying RPC protocol, and generally
+    /// responsible for any protocol-specific data frame handling.
     pub trait Codec: Send + Sync + 'static {
         /// Encodes notify frame
         fn encode_notify(
@@ -94,26 +158,14 @@ pub mod codec {
         }
 
         /// Encodes response frame
-        ///
-        fn encode_response_okay(
+        fn encode_response(
             &self,
             req_id: &[u8],
+            is_error: bool,
             response: &dyn Serialize,
         ) -> Result<(), EncodeError> {
-            let _ = (req_id, response);
+            let _ = (req_id, response, is_error);
             Err(EncodeError::UnsupportedFeature("Response is not supported by this codec".into()))
-        }
-
-        ///
-        fn encode_response_error(
-            &self,
-            req_id: &[u8],
-            error: &dyn Serialize,
-        ) -> Result<(), EncodeError> {
-            let _ = (req_id, error);
-            Err(EncodeError::UnsupportedFeature(
-                "Response with error is not supported by this codec".into(),
-            ))
         }
 
         /// Decodes inbound frame, and identifies the frame type.
@@ -169,8 +221,7 @@ pub mod codec {
     pub enum InboundFrameType {
         Notify { method: Range<usize> },
         Request { method: Range<usize>, req_id: Range<usize> },
-        Response { req_id_hash: u64 },
-        ResponseError { req_id_hash: u64 },
+        Response { req_id_hash: u64, is_error: bool },
     }
 }
 
