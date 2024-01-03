2c125ff5d4b015276d9cdd37a7059658011fca4a
kang-sw <ki6080@gmail.com>
2024-01-03 11:44:09 +0900

    lib: declare core types

--- src/lib.rs
+++ src/lib.rs
@@ -1,159 +1,204 @@
-pub mod builder {
-    use std::sync::Arc;
-    use tokio::sync::Mutex as AsyncMutex;
-
-    use crate::req::RequestStorage;
-
-    pub struct Builder<Codec, Router, UserData, Write, Read> {
-        codec: Codec,
-        router: Router,
-        user_data: UserData,
-        write: Write,
-        read: Read,
-        reqs: Option<RequestStorage>,
-    }
-
-    pub(crate) struct ClientImpl<Codec, UserData, Write> {
-        codec: Codec,
-        user_data: UserData,
-        write: Arc<AsyncMutex<Write>>,
-    }
-
-    /// Creates new builder with empty settings.
-    ///
-    /// A tokio runtime must be running.
-    pub fn builder() -> Builder<(), (), (), (), ()> {
-        Builder { codec: (), router: (), user_data: (), write: (), read: (), reqs: None }
-    }
-
-    impl<T0, T1, T2, T3, T4> Builder<T0, T1, T2, T3, T4> {
-        // TODO: Builder struct implementations
-    }
-
-    impl<Codec, UserData, Write> crate::client::Client for ClientImpl<Codec, UserData, Write>
-    where
-        UserData: Send + Sync + 'static,
-    {
-        type UserData = UserData;
-    }
-
-    // TODO: implement receiver task
-}
-pub mod backend {
-    use std::future::Future;
-
-    use bytes::Bytes;
-    use thiserror::Error;
-
-    pub trait Backend: Send + Sync + 'static {
-        /// Spawn new asynchronous task. Must be succeeded.
-        fn spawn_task(&self, task: impl Future<Output = ()> + Send);
-    }
-}
-pub mod client {
-    use std::sync::Arc;
-
-    use crate::req::RequestStorage;
-
-    pub(crate) trait Client {
-        type UserData: Send + Sync + 'static;
-    }
-
-    /// A client that only sends notifications
-    #[derive(Clone)]
-    pub struct NotifyHandle<UserData> {
-        client: Arc<dyn Client<UserData = UserData>>,
-    }
-
-    /// A client which can send requests and receive replies
-    #[derive(Clone)]
-    pub struct Handle<UserData> {
-        noti: NotifyHandle<UserData>,
-        reqs: Arc<RequestStorage>,
-    }
-}
+pub mod rpc;
 pub mod codec {
-    use std::num::NonZeroU64;
+    use bytes::BytesMut;
+    use erased_serde::Deserializer as DynDeserializer;
+    use erased_serde::Serialize as DynSerialize;
 
-    pub trait Codec {
-        /// Returns unique hash of this codec. If notification encoding is not deterministic,
-        /// returns `None`. Otherwise, if two codecs return the same hash, their encoded
-        /// notifications are guaranteed to be equal, which is allowed to be reused over multiple
-        /// clients.
-        ///
-        /// This values is used when broadcasting notification message over multiple clients.
-        fn notify_type_id(&self) -> Option<NonZeroU64> {
-            None
+    pub trait Codec: 'static + Send + Sync {
+        fn encode_notify(
+            &self,
+            method: &str,
+            params: &dyn DynSerialize,
+            buf: &mut BytesMut,
+        ) -> Result<(), EncodeError> {
+            let _ = (method, params, buf);
+            Err(EncodeError::UnsupportedAction)
         }
 
-        // TODO: Encode notify
-
-        // TODO: Encode request
-
-        // TODO: Encode reply OK / Error
-
-        // TODO: Decode inbound -> determine type
-
-        // TODO: Retrieve payload from inbound
-    }
-}
-pub mod io {}
-pub mod msg {
-    pub struct RequestBody {}
-}
-pub mod req {
-    pub struct RequestStorage {
-        // TODO: List of awaiting requests
-
-        // TODO: Is reader closed ?
-    }
-}
-pub mod recv {
-    pub trait Router {
-        // TODO: Handle Error
-
-        // TODO: Handle Request
-
-        // TODO: Handle Notify
+        /// Encodes a request message into the given buffer. The `request_id` is contextually unique
+        /// integer value, which is used to match the returned response with the request that was
+        /// sent. Underlying implementation can encode this `request_id` in any manner or type, as
+        /// long as it can be decoded back to the original value.
+        fn encode_request(
+            &self,
+            request_id: RequestId,
+            method: &str,
+            params: &dyn DynSerialize,
+            buf: &mut BytesMut,
+        ) -> Result<(), EncodeError> {
+            let _ = (request_id, method, params, buf);
+            Err(EncodeError::UnsupportedAction)
+        }
     }
 
-    impl Router for () {}
+    pub mod error {
+        use thiserror::Error;
+
+        #[derive(Debug, Error)]
+        pub enum EncodeError {
+            #[error("Unsupported type of action")]
+            UnsupportedAction,
+        }
+    }
+
+    use crate::defs::RequestId;
+
+    use self::error::*;
+}
+pub mod io {
+    use std::{
+        pin::Pin,
+        task::{Context, Poll},
+    };
+
+    use bytes::{Buf, Bytes};
+    use tokio::io::AsyncWrite;
+
+    /// [`AsyncFrameWrite`] is a trait defining the interface for writing data frames
+    /// to the underlying transport. This trait can either be a straightforward wrapper
+    /// around the `AsyncWrite` interface or an optimized custom implementation.
+    /// It may collect [`Bytes`] and flush them in batches to minimize buffer copies.
+    pub trait AsyncFrameWrite: 'static + Send {
+        /// Notifies the underlying transport about a new frame.
+        fn start_frame(&mut self) -> std::io::Result<()> {
+            Ok(())
+        }
+
+        /// Writes a frame to the underlying transport.
+        ///
+        /// Type of the parameter `buffer` is reference to help this method to easily deal with
+        /// remaining byte count in the buffer.
+        fn poll_write_frame(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buffer: &mut Bytes,
+        ) -> Poll<std::io::Result<()>>;
+
+        /// Flushes the underlying transport, writing any pending data.
+        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>>;
+
+        /// Closes the underlying transport.
+        fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>>;
+    }
+
+    /// [`AsyncFrameRead`] is a trait defining the interface for reading data frames from the
+    /// underlying transport.
+    pub trait AsyncFrameRead {
+        /// Reads a single frame. The returned frame must be a *complete* message that can be
+        /// decoded by a single action. Therefore, the responsibility of framing inbound stream data
+        /// lies with the implementation of [`AsyncFrameRead`].
+        fn poll_read_frame(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+        ) -> Poll<std::io::Result<Bytes>>;
+    }
+
+    // ========================================================== AsyncWriteFrame ===|
+
+    /// Implements [`AsyncFrameWrite`] for any type that implements [`tokio::io::AsyncWrite`].
+    impl<T> AsyncFrameWrite for T
+    where
+        T: AsyncWrite + Send + 'static,
+    {
+        fn poll_write_frame(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &mut Bytes,
+        ) -> Poll<std::io::Result<()>> {
+            match AsyncWrite::poll_write(self, cx, buf.as_ref())? {
+                Poll::Ready(x) => {
+                    buf.advance(x);
+                    Poll::Ready(Ok(()))
+                }
+                Poll::Pending => Poll::Pending,
+            }
+        }
+
+        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+            AsyncWrite::poll_flush(self, cx)
+        }
+
+        fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+            AsyncWrite::poll_shutdown(self, cx)
+        }
+    }
 }
 pub mod defs {
-    pub(crate) type DataSize = u32;
-    type ReqIdType = u32;
-    pub(crate) type DataRange = std::ops::Range<DataSize>;
 
-    macro_rules! __define_integer_id {
-		($(#[doc = $s:literal])* $vis:vis struct $ident:ident($ty:ty)) => {
-			$(#[doc = $s])*
+    // ========================================================== Basic Types ===|
+
+    use std::ops::Range;
+
+    pub(crate) type SizeType = u32;
+
+    /// 32-bit range type. Defines set of helper methods for working with ranges.
+    #[derive(Clone, Copy)]
+    pub(crate) struct RangeType([SizeType; 2]);
+
+    // ==== RangeType ====
+
+    impl From<Range<usize>> for RangeType {
+        fn from(value: Range<usize>) -> Self {
+            Self([value.start as SizeType, value.end as SizeType])
+        }
+    }
+
+    impl From<RangeType> for Range<usize> {
+        fn from(value: RangeType) -> Self {
+            Self { start: value.0[0] as usize, end: value.0[1] as usize }
+        }
+    }
+
+    impl RangeType {
+        pub fn new(start: usize, end: usize) -> Self {
+            Self([start as SizeType, end as SizeType])
+        }
+
+        pub fn r(&self) -> Range<usize> {
+            (*self).into()
+        }
+    }
+
+    // ========================================================== ID Types ===|
+
+    macro_rules! define_id {
+		($(#[doc=$doc:literal])* $vis:vis struct $name:ident($base:ty)) => {
+			$(#[doc=$doc])*
 			#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-			$vis struct $ident($ty);
+			$vis struct $name($base);
 
-			impl $ident {
-				#[inline]
-				pub fn new(id: $ty) -> Self {
-					Self(id)
-				}
+            impl $name {
+                #[allow(dead_code)]
+                pub(crate) fn new(value: $base) -> Self {
+                    Self(value)
+                }
 
-				#[inline]
-				pub fn get(&self) -> $ty {
-					self.0
-				}
-			}
+                pub fn value(&self) -> $base {
+                    self.0
+                }
+            }
 		};
 	}
 
-    __define_integer_id!(
-        /// The in-memory representation of a request ID.
-        pub struct RequestId(ReqIdType)
-    );
+    define_id! {
+        /// Unique identifier of a RPC request.
+        pub struct RequestId(u32)
+    }
 }
 
-pub use backend::Backend;
-pub use client::{Handle, NotifyHandle};
+pub mod ext_io {}
+pub mod ext_codec {
+    //! Implementations of RPC codecs ([`Codec`]) for various protocols
 
-// ======== Re-exports ======== //
+    #[cfg(feature = "jsonrpc")]
+    pub mod jsonrpc {}
+    #[cfg(feature = "mspack-rpc")]
+    pub mod msgpackrpc {}
+    #[cfg(feature = "mspack-rpc-postcard")]
+    pub mod msgpackrpc_postcard {}
+}
+
+// ========================================================== Re-exports ===|
 
 pub extern crate erased_serde;
 pub extern crate serde;
---
+++ src/rpc.rs
@@ -0,0 +1,357 @@
+use std::borrow::Cow;
+use std::future::poll_fn;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::sync::Weak;
+
+use bytes::Bytes;
+use tokio::sync::Mutex as AsyncMutex;
+
+// ==== Basic RPC ====
+
+/// Generic trait for underlying RPC connection.
+pub(crate) trait RpcContext<U: RpcUserData>: std::fmt::Debug {
+    fn self_as_codec(&self) -> Arc<dyn Codec>;
+    fn codec(&self) -> &dyn Codec;
+    fn user_data(&self) -> &U;
+    fn writer(&self) -> &AsyncMutex<dyn AsyncFrameWrite>;
+}
+
+/// A trait constraint for user data type of a RPC connection.
+pub trait RpcUserData: Send + Sync + 'static {}
+impl<T> RpcUserData for T where T: Send + Sync + 'static {}
+
+/// A RPC client handle which can only send RPC notifications.
+///
+/// `NotifyClient` is lightweight handle to the underlying RPC connection.
+#[derive(Debug)]
+pub struct NotifyClient<U> {
+    inner: Arc<dyn RpcContext<U>>,
+    tx_deferred: mpsc::Sender<DeferredDirective>,
+}
+
+/// A weak RPC client handle which can only send RPC notifications.
+///
+/// Should be upgraded to [`NotifyClient`] before use.
+#[derive(Debug)]
+pub struct WeakNotifyClient<U> {
+    inner: Weak<dyn RpcContext<U>>,
+}
+
+// ==== Request Capability ====
+
+/// A RPC client handle which can send RPC requests and notifications.
+#[derive(Debug)]
+pub struct Client<U> {
+    inner: NotifyClient<U>,
+    req: Arc<RequestContext>,
+}
+
+/// A weak RPC client handle which can send RPC requests and notifications.
+///
+/// Should be upgraded to [`Client`] before use.
+struct WeakClient<U> {
+    inner: Weak<dyn RpcContext<U>>,
+    req: Weak<RequestContext>,
+}
+
+/// A context for pending RPC requests.
+#[derive(Debug)]
+struct RequestContext {
+    // TODO: pending task hash map
+}
+
+/// An awaitable response for a sent RPC request
+pub struct ReceiveResponse<'a, U> {
+    owner: Cow<'a, Client<U>>,
+}
+
+// ==== Definitions ====
+
+/// Error type definitions
+pub mod error {
+    use bytes::Bytes;
+    use thiserror::Error;
+    use tokio::sync::mpsc::error::TrySendError;
+
+    use crate::codec;
+
+    use super::driver::DeferredDirective;
+
+    #[derive(Debug, Error)]
+    pub enum SendError {
+        #[error("Encoding failed: {0}")]
+        EncodeFailed(#[from] codec::error::EncodeError),
+
+        /// This won't be returned calling `*_deferred` methods.
+        #[error("Async IO failed: {0}")]
+        AsyncIoError(#[from] std::io::Error),
+
+        /// This won't be returned calling async methods.
+        #[error("Failed to send request to background driver: {0}")]
+        DeferredIoError(#[from] DeferredActionError<Bytes>),
+    }
+
+    #[derive(Debug, Error)]
+    pub enum RequestError {}
+
+    #[derive(Debug, Error)]
+    pub enum DeferredActionError<T> {
+        #[error("Background runner is already closed!")]
+        BackgroundRunnerClosed,
+
+        #[error("Channel is at capacity!")]
+        ChannelAtCapacity(T),
+    }
+
+    // ==== DeferredActionError ====
+
+    pub(crate) fn convert_deferred_write_err(
+        e: TrySendError<DeferredDirective>,
+    ) -> DeferredActionError<Bytes> {
+        match e {
+            TrySendError::Closed(_) => DeferredActionError::BackgroundRunnerClosed,
+            TrySendError::Full(DeferredDirective::WriteNotify(x)) => {
+                DeferredActionError::ChannelAtCapacity(x)
+            }
+            TrySendError::Full(_) => unreachable!(),
+        }
+    }
+
+    pub(crate) fn convert_deferred_action_err(
+        e: TrySendError<DeferredDirective>,
+    ) -> DeferredActionError<()> {
+        match e {
+            TrySendError::Closed(_) => DeferredActionError::BackgroundRunnerClosed,
+            TrySendError::Full(_) => DeferredActionError::ChannelAtCapacity(()),
+        }
+    }
+}
+
+use bytes::BytesMut;
+use tokio::sync::mpsc;
+
+use crate::codec::Codec;
+use crate::io::AsyncFrameWrite;
+
+use self::error::*;
+
+pub mod msg {
+    use std::{future::Future, sync::Arc};
+
+    use bytes::Bytes;
+    use serde::de::Deserialize;
+
+    use crate::codec::Codec;
+
+    use super::{ReceiveResponse, RpcUserData};
+
+    /// Response message from RPC server.
+    pub struct Response(Arc<dyn Codec>, Bytes);
+
+    // ======== Response ======== //
+
+    impl<'a, U: RpcUserData> Future for ReceiveResponse<'a, U> {
+        type Output = Response;
+
+        fn poll(
+            self: std::pin::Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Self::Output> {
+            todo!("")
+        }
+    }
+
+    impl Response {
+        pub fn parse<R>(&self) -> erased_serde::Result<R>
+        where
+            R: for<'de> Deserialize<'de>,
+        {
+            todo!()
+        }
+    }
+}
+
+pub mod builder {
+    //! # Builder for RPC connection
+
+    ///
+    pub struct Builder<Wr, Rd, U, C> {
+        _0: std::marker::PhantomData<(Wr, Rd, U, C)>,
+    }
+}
+
+mod driver {
+    use bytes::Bytes;
+
+    use crate::defs::RequestId;
+
+    /// A message to be sent to the background dedicated writer task.
+    pub(crate) enum DeferredDirective {
+        /// Close the writer transport immediately after receiving this message.
+        CloseImmediately,
+
+        /// Close the writer transport after flushing all pending write requests.
+        ///
+        /// The rx channel, which is used to receive this message, will be closed right after this
+        /// message is received.
+        CloseAfterFlush,
+
+        /// Flush the writer transport.
+        Flush,
+
+        /// Write a notification message.
+        WriteNotify(Bytes),
+
+        /// Write a request message. If the sending of the request is aborted by the writer, the
+        /// request message will be revoked and will wake up the pending task.
+        WriteRequest(Bytes, RequestId),
+    }
+}
+
+use driver::*;
+
+// ========================================================== RpcContext ===|
+
+impl<U> dyn RpcContext<U> {
+    pub(crate) async fn write_frame(&self, buf: Bytes) -> Result<(), SendError> {
+        todo!()
+    }
+}
+
+// ========================================================== NotifyClient ===|
+
+/// Implements notification methods for [`NotifyClient`].
+impl<U: RpcUserData> NotifyClient<U> {
+    /// Send a RPC notification.
+    pub async fn notify<T: serde::Serialize>(
+        &self,
+        buf: &mut BytesMut,
+        method: &str,
+        params: &T,
+    ) -> Result<(), SendError> {
+        buf.clear();
+        self.inner.codec().encode_notify(method, params, buf)?;
+        self.inner.write_frame(buf.split().freeze()).await
+    }
+
+    pub fn notify_deferred<T: serde::Serialize>(
+        &self,
+        buf: &mut BytesMut,
+        method: &str,
+        params: &T,
+    ) -> Result<(), SendError> {
+        buf.clear();
+        self.inner.codec().encode_notify(method, params, buf)?;
+        self.write_frame_deferred(buf.split().freeze())?;
+
+        Ok(())
+    }
+
+    fn write_frame_deferred(&self, buf: Bytes) -> Result<(), DeferredActionError<Bytes>> {
+        self.tx_deferred
+            .try_send(DeferredDirective::WriteNotify(buf))
+            .map_err(error::convert_deferred_write_err)
+    }
+
+    pub fn user_data(&self) -> &U {
+        self.inner.user_data()
+    }
+
+    pub fn codec(&self) -> &dyn Codec {
+        self.inner.codec()
+    }
+
+    pub fn cloned_codec(&self) -> Arc<dyn Codec> {
+        self.inner.self_as_codec()
+    }
+
+    /// Que a close request to the background writer task. It will first flush all remaining data
+    /// transfer request, then will close the writer. If background channel is already closed,
+    /// returns `Err`.
+    ///
+    /// If `drop_after_this` is specified, any deferred outbound message will be dropped.
+    pub fn close_writer(&self, drop_after_this: bool) -> Result<(), DeferredActionError<()>> {
+        self.tx_deferred
+            .try_send(if drop_after_this {
+                DeferredDirective::CloseImmediately
+            } else {
+                DeferredDirective::CloseAfterFlush
+            })
+            .map_err(error::convert_deferred_action_err)
+    }
+
+    /// Closes the writer channel immediately. This will invalidate all pending write requests.
+    pub async fn force_close_writer(self) -> std::io::Result<()> {
+        let mut writer = self.inner.writer().lock().await;
+
+        // SAFETY: `writer` memory goes nowhere during locked.
+        poll_fn(|cx| unsafe { Pin::new_unchecked(&mut *writer).poll_close(cx) }).await
+    }
+
+    pub async fn flush_writer(&self) -> std::io::Result<()> {
+        let mut writer = self.inner.writer().lock().await;
+
+        // SAFETY: `writer` memory goes nowhere during locked.
+        poll_fn(|cx| unsafe { Pin::new_unchecked(&mut *writer).poll_flush(cx) }).await
+    }
+
+    /// Requests flush to the background writer task. As actual flush operation is done in
+    /// background writer task, you can't get the actual result of the flush operation.
+    pub fn flush_writer_deferred(&self) -> Result<(), DeferredActionError<()>> {
+        self.tx_deferred
+            .try_send(DeferredDirective::Flush)
+            .map_err(error::convert_deferred_action_err)
+    }
+}
+
+impl<U> Clone for NotifyClient<U> {
+    fn clone(&self) -> Self {
+        Self { inner: self.inner.clone(), tx_deferred: self.tx_deferred.clone() }
+    }
+}
+
+// ========================================================== Client ===|
+
+/// Implements request methods for [`Client`].
+impl<U: RpcUserData> Client<U> {
+    pub async fn request<T: serde::Serialize>(
+        &self,
+        method: &str,
+        params: &T,
+    ) -> Result<ReceiveResponse<U>, RequestError> {
+        todo!()
+    }
+
+    pub fn request_deferred<T: serde::Serialize>(
+        &self,
+        method: &str,
+        params: &T,
+    ) -> Result<ReceiveResponse<U>, RequestError> {
+        todo!()
+    }
+}
+
+impl<U> Clone for Client<U> {
+    fn clone(&self) -> Self {
+        Self { inner: self.inner.clone(), req: self.req.clone() }
+    }
+}
+
+/// Provides handy way to access [`NotifyClient`] methods in [`Client`].
+impl<U> std::ops::Deref for Client<U> {
+    type Target = NotifyClient<U>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+// ======== ReceiveResponse ======== //
+
+impl<'a, U> ReceiveResponse<'a, U> {
+    /// Elevate the lifetime of the response to `'static`.
+    pub fn into_owned(self) -> ReceiveResponse<'static, U> {
+        ReceiveResponse { owner: Cow::Owned(self.owner.into_owned()) }
+    }
+}
