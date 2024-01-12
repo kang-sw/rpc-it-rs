272c2b7b48bd8217a4c41b4a0fedcf0099828e29
kang-sw <ki6080@gmail.com>
2024-01-12 17:11:34 +0900

    core: verify multiple request test

--- tests/basic_api_jsonrpc.rs
+++ tests/basic_api_jsonrpc.rs
@@ -28,8 +28,7 @@ fn verify_notify() {
         .build_write_only();
 
     let task_send = async {
-        let mut buf = BytesMut::new();
-        let b = &mut buf;
+        let b = &mut BytesMut::new();
 
         tx_rpc
             .notify(b, "test", &serde_json::json!({ "test": "test" }))
@@ -158,29 +157,35 @@ fn verify_request() {
                 let b = &mut BytesMut::new();
 
                 // Test plain request response
-                println!("client: sending request");
+                println!("I.client: sending request");
                 let response = client.request(b, "hello", &(1, 2)).await.unwrap();
 
-                println!("server: receiving request");
+                println!("I.server: receiving request");
                 let request = server.recv().await.unwrap();
 
                 assert_eq!(request.method(), "hello");
                 let (x1, x2) = request.parse::<(i32, i32)>().unwrap();
                 assert_eq!((x1, x2), (1, 2));
 
-                println!("server: sending response");
+                println!("I.server: sending response");
                 request.response(b, Ok(x1 + x2)).await.unwrap();
 
-                println!("client: receiving response");
+                println!("I.client: receiving response");
                 let response = response.await.unwrap();
 
                 assert_eq!(response.parse::<i32>().unwrap(), 3);
 
                 // Test dropped request
+                println!("I.client: sending request 2");
                 let response = client.request(b, "hello", &(3, 4)).await.unwrap();
+
+                println!("I.server: receiving request 2");
                 let request = server.recv().await.unwrap();
+
+                println!("I.server: dropping request 2");
                 drop(request);
 
+                println!("I.client: receiving response 2");
                 let err = response.await.unwrap_err();
                 let ReceiveResponseError::ErrorResponse(err) = err else {
                     panic!()
@@ -188,19 +193,68 @@ fn verify_request() {
 
                 assert_eq!(err.errc(), ResponseError::Unhandled);
 
+                println!("I: done");
                 drop(tc);
             })
             .unwrap();
     }
 
+    executor.run_until_stalled();
+
     {
         let client = client.clone();
+        let tc = test_counter.clone();
+
+        spawner
+            .spawn(async move {
+                // TODO: test multiple requests
+                let b = &mut BytesMut::new();
+                let req1 = client.request(b, "pewpew", &(1, 2)).await.unwrap();
+                let req2 = client.request(b, "tewtew", &(3, 4)).await.unwrap();
+                let req3 = client.request(b, "rewrew", &(5, 6)).await.unwrap();
+
+                let rep1 = req1.await.unwrap().parse::<i32>().unwrap();
+                let rep2 = req2.await.unwrap().parse::<i32>().unwrap();
+                let rep3 = req3.await.unwrap().parse::<i32>().unwrap();
+
+                assert_eq!(rep1, 3);
+                assert_eq!(rep2, 7);
+                assert_eq!(rep3, 11);
+
+                println!("II.client: done");
+                drop(tc);
+            })
+            .unwrap();
+
         let server = server.clone();
         let tc = test_counter.clone();
 
         spawner
             .spawn(async move {
-                // TODO: test notify / request mix
+                let b = &mut BytesMut::new();
+
+                let req1 = server.recv().await.unwrap();
+                let req2 = server.recv().await.unwrap();
+                let req3 = server.recv().await.unwrap();
+
+                assert_eq!(req1.method(), "pewpew");
+                assert_eq!(req2.method(), "tewtew");
+                assert_eq!(req3.method(), "rewrew");
+
+                let (x1, x2) = req1.parse::<(i32, i32)>().unwrap();
+                let (x3, x4) = req2.parse::<(i32, i32)>().unwrap();
+                let (x5, x6) = req3.parse::<(i32, i32)>().unwrap();
+
+                assert_eq!((x1, x2), (1, 2));
+                assert_eq!((x3, x4), (3, 4));
+                assert_eq!((x5, x6), (5, 6));
+
+                req1.response(b, Ok(x1 + x2)).await.unwrap();
+                req2.response(b, Ok(x3 + x4)).await.unwrap();
+                req3.response(b, Ok(x5 + x6)).await.unwrap();
+
+                println!("II.server: done");
+                drop(tc);
             })
             .unwrap();
     }
